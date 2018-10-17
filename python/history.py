cb3d423270345b0ac0f37996f0efe5176be6b429
Alex moore <alex.moore@mediciventures.com>
2018-10-17 12:59:27 -0600

    Backup

--- python/history.py
+++ python/history.py
@@ -2,3 +2,10 @@ import readline
 def history():
     for i in range(readline.get_current_history_length()):
         print(readline.get_history_item(i + 1))
+
+def save_history(filename):
+    hist = []
+    for i in range(readline.get_current_history_length()):
+        hist.append(readline.get_history_item(i + 1))
+    with open(filename, 'w') as stream:
+        stream.write("\n".join(hist))
