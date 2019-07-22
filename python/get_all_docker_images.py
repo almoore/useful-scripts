5fd535b138fc8d4946b41df42af6eebf59d9c520
Alex Moore <alexander.g.moore1@gmail.com>
2019-07-22 11:10:14 -0600

    Add script for grabbing all docker images hashes in a remote registry

---
+++ python/get_all_docker_images.py
@@ -0,0 +1,55 @@
+import json
+import os
+import sys
+import requests
+import docker
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('reg_name', action='store', help='The regisrty to query')
+    return parser.parse_args()
+    
+
+def get_client():
+    global client
+    if 'client' in globals() and isinstance(client, docker.client.DockerClient):
+        return client
+    else:
+        print("Creating docker client from env")
+        client = docker.from_env()
+        return client
+
+
+def get_tags(url, paginate=True):
+    response = requests.get(url)
+    if response.ok:
+        data = response.json()
+        tags = data.get('results', [])
+        _next = data.get('next')
+        if _next and paginate:
+            print("Getting next: {}".format(_next)) 
+            tags += get_tags(_next)
+    else:
+        tags = []
+    return tags
+
+
+def get_remote_id(name):
+    client = get_client()
+    print("Getting hash id for {}".format(name))
+    rd = client.images.get_registry_data(name)
+    return rd.id
+
+
+def main():
+    reg_name = 'library/{}'.format(args.reg_name) if len(args.reg_name.split('/') == 1 else args.reg_name
+    url = 'https://hub.docker.com/v2/repositories/{}/tags/'.format(reg_name)
+    tags = get_tags(url)
+    images = [':'.join([reg_name, t['name']]) for t in tags]
+    ids = [get_remote_id(i) for i in images]
+    print('images = {}'.format(json.dumps(ids, indent=2)))
+
+if __name__ == '__main__':
+    main()
