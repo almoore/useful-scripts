93f3896cdd79aa1b5f870ac0f7501a9c1c6507f5
Alex Moore <alex.moore@anthem.com>
2021-12-20 15:29:44 -0700

    Add aws script to download all policies and roles

---
+++ aws/download-policies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [ -z "$POLICY_ARNS" ]; then
+  POLICY_ARNS=$(aws iam list-policies --query "Policies[].Arn" --output text)
+fi
+AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+mkdir -p aws-iam/arn:aws:iam::aws:policy
+for ARN in ${POLICY_ARNS}; do
+    if ! echo $ARN | grep $AWS_ACCOUNT; then
+        continue
+    fi
+    _DIR=$(dirname ${ARN})
+    mkdir -p aws-iam/${_DIR}
+    echo -n "Getting policy ${ARN} "
+    V=$(aws iam get-policy --policy-arn ${ARN} --query Policy.DefaultVersionId --output text)
+    echo $V
+    aws iam get-policy-version --policy-arn ${ARN} --version-id $V --query PolicyVersion.Document > "aws-iam/${ARN}.json"
+done
