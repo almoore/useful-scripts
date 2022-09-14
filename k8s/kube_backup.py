482905cc354e131de43357694dd435aaafe94f23
Alex Moore <alex.moore@anthem.com>
2022-09-14 15:12:28 -0600

    Update k8s_backup.py

--- k8s/kube_backup.py
+++ k8s/kube_backup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 import os
-import tempfile
 import yaml
 import argparse
 from subprocess import Popen, PIPE, STDOUT
-
-release_data = None
 
 
 class bcolors:
@@ -27,13 +24,14 @@ def parse_args():
                         help="The namespace to use this will default to what is in the release file")
     parser.add_argument("-q", "--quiet", action="store_true", help="Suppress context and display short command summary")
     parser.add_argument("-k", "--kinds",
-                        default="all,jobs,cronjobs,ingresses,endpoints,customresourcedefinitions,configmaps,secrets,configmap,events,pvc,serviceaccount,clusterrole,clusterrolebindings,storageclass,hrs",
+                        # default="all,jobs,cronjobs,ingresses,endpoints,customresourcedefinitions,configmaps,secrets,configmap,events,pvc,serviceaccount,clusterrole,clusterrolebindings,storageclass,hrs",
                         help="comma seperated list of kinds of object to restrict the dump to example: configmaps,secrets,events,pods")
     parser.add_argument("-a", "--all", action="store_true", help="if present, collects from all namespace and kinds")
     parser.add_argument("-d", "--output-directory",
-                        default="k8s-backup",
                         help="directory to output files; defaults to k8s-backup/<cluster-dir>")
-    # parser.add_argument("-z", "--archive", action="store_true", help="if present, archives and removes the output directory")
+    # parser.add_argument("--debug", action="store_true", help="Enable debug logs")
+    parser.add_argument("-z", "--archive", action="store_true", help="if present, archives and removes the output directory")
+    parser.add_argument("-s", "--summary", action="store_true", help="Store a summary file as resources.yaml")
     return parser.parse_args()
 
 
@@ -57,9 +55,9 @@ def shell_out(command):
     :param command:
     :return:
     """
-    process = Popen(command, stdout=PIPE, stderr=STDOUT, shell=True)
-    stdout_data = process.communicate()[0]
-    return stdout_data, process.returncode
+    process = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = process.communicate(timeout=360) # timout in seconds
+    return stdout, stderr, process.returncode
 
 
 def get_context(ns=None):
@@ -76,7 +74,20 @@ def get_context(ns=None):
 
 
 def get_all_kinds():
-    return ",".join(run("kubectl api-resources  -o name"))
+    return ",".join(run("kubectl api-resources -o name"))
+
+
+def get_custom_resource_definitions(all=False, namespace=None):
+    if all:
+        ns_cmd = "-A"
+    elif namespace is not None:
+        ns_cmd = f"--namespace {namespace}"
+    print(f"{bcolors.OKGREEN}Retrieving custom resource configurations{bcolors.ENDC}")
+    custom_resources = ",".join(run("kubectl get customresourcedefinitions -o name"))
+    if custom_resources == "":
+        return (None, None, None)
+    command = f"kubectl get {custom_resources} {ns_cmd} -o yaml"
+    return shell_out(command)
 
 
 def split_resources(data, quiet=False):
@@ -111,7 +122,11 @@ def main():
     ctx, ns = get_context(ns=namespace)
     if args.kinds is None:
         args.kinds = get_all_kinds()
-    output_dir = os.path.join(args.output_directory, ctx)
+    output_dir = args.output_directory
+    if output_dir is None:
+        default="k8s-backup",
+        output_dir = os.path.join("k8s-backup", ctx)
+    output_dir = os.path.expanduser(output_dir)
     print(f"{bcolors.OKGREEN}Creating output in: {output_dir}{bcolors.ENDC}")
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -128,9 +143,32 @@ def main():
 
     if not args.quiet:
         print(f"{bcolors.BOLD}{bcolors.OKGREEN}Using the context {ctx}, namespace {ns}{bcolors.ENDC}")
-    text, ret = shell_out(command)
+    text, err, ret = shell_out(command)
+    print(f'{bcolors.BOLD}{bcolors.YELLOW}{err}{bcolors.ENDC}')
+    if args.summary:
+        with open("resources.yaml", "wb") as fs:
+            fs.write(text)
+        with open("errors.txt", "wb") as fs:
+            fs.write(err)
     resources = yaml.safe_load(text)
     split_resources(resources, args.quiet)
+    text, err, ret = get_custom_resource_definitions(all=args.all, namespace=args.namespace)
+    print(f'{bcolors.BOLD}{bcolors.YELLOW}{err}{bcolors.ENDC}')
+    if args.summary:
+        with open("custom-resources.yaml", "wb") as fs:
+            fs.write(text)
+        with open("custom-resources-errors.txt", "wb") as fs:
+            fs.write(err)
+    resources = yaml.safe_load(text)
+    split_resources(resources, args.quiet)
+    if args.archive:
+        zip_dir = os.getcwd()
+        os.chdir('..')
+        import shutil
+        import time
+        ts = time.time()
+        shutil.make_archive(f"{ctx}_{ts}", "zip", zip_dir)
+        shutil.rmtree(zip_dir)
 
 
 if __name__ == "__main__":
