51dd01faa90b5d48b2f3c3e2814e2d15f4c612cb
Alex Moore <alexander.g.moore1@gmail.com>
2025-04-23 12:26:13 -0600

    Update facebook_photo_to_wordpress.py and formating using black

--- python/wordpress_media_upload.py
+++ python/wordpress_media_upload.py
@@ -1,16 +1,17 @@
 import requests
 import os
 
+
 def upload_image_to_wordpress_com(access_token, site_id, image_path):
-    url = f'https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/media/new'
+    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/media/new"
 
     try:
-        with open(image_path, 'rb') as img_file:
+        with open(image_path, "rb") as img_file:
             headers = {
-                'Authorization': f'Bearer {access_token}',
+                "Authorization": f"Bearer {access_token}",
             }
             files = {
-               'media[]': img_file,
+                "media[]": img_file,
             }
             response = requests.post(url, headers=headers, files=files)
         if response.status_code == 201:
@@ -24,28 +25,27 @@ def upload_image_to_wordpress_com(access_token, site_id, image_path):
         print(f"An error occurred: {e}")
 
 
-
 def get_wordpress_com_site_id(site_url, access_token):
     """Retrieve the site ID for a given WordPress.com site URL."""
     endpoint = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_url}"
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+    headers = {"Authorization": f"Bearer {access_token}"}
 
     response = requests.get(endpoint, headers=headers)
 
     if response.status_code == 200:
         site_data = response.json()
-        return site_data.get('ID')  # The key is 'ID' in this API version
+        return site_data.get("ID")  # The key is 'ID' in this API version
     else:
-        raise Exception(f"Failed to retrieve site ID: {response.status_code} {response.text}")
+        raise Exception(
+            f"Failed to retrieve site ID: {response.status_code} {response.text}"
+        )
 
 
 # Example usage
-access_token = os.environ.get('ACCESS_TOKEN')
-site_url = os.environ.get('SITE_URL')
+access_token = os.environ.get("ACCESS_TOKEN")
+site_url = os.environ.get("SITE_URL")
 site_id = get_wordpress_com_site_id(site_url, access_token)
 print(f"site_id: {site_id}")
-image_path = os.environ.get('IMAGE_PATH', 'image.jpg') # path to your image.jpg
+image_path = os.environ.get("IMAGE_PATH", "image.jpg")  # path to your image.jpg
 
 upload_image_to_wordpress_com(access_token, site_id, image_path)
