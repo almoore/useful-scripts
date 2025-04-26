af9c1d426e16c4364fef6dbe639249941dd8d9bc
Alex Moore <alexander.g.moore1@gmail.com>
2025-04-25 18:25:10 -0600

    Update wordpress photo script and pipfile changes

--- python/facebook_photo_to_wordpress.py
+++ python/facebook_photo_to_wordpress.py
@@ -4,11 +4,11 @@ import requests
 import argparse
 import os
 import json
+import html
 from io import BytesIO
 from datetime import datetime
 from urllib.parse import urlparse
-
-from python.wordpress_media_upload import access_token
+import urllib3.util
 
 
 def parseargs():
@@ -19,6 +19,9 @@ def parseargs():
     parser.add_argument("--post", default=os.environ.get("WORDPRESS_POST_ID"))
     parser.add_argument(
         "--export-file", help="Facebook json export file with a list of posts and media"
+    )
+    parser.add_argument(
+        "--wordpress-posts-file", help="Wordpress json file with a list of posts"
     )
     parser.add_argument(
         "--url",
@@ -44,7 +47,11 @@ def parseargs():
         default=os.environ.get("WORDPRESS_ACCESS_TOKEN"),
         help="Wordpress OAuth2 token.",
     )
-
+    parser.add_argument(
+        "--photo-dir",
+        default=os.environ.get("."),
+        help="Local location for photos",
+    )
     parser.add_argument(
         "--start-date",
         default="2005-01-01",
@@ -59,20 +66,12 @@ def parseargs():
     return parser.parse_args()
 
 
-def get_post_by_title(site_id, access_token, post_title):
+def get_wordpress_post_by_title(site_id, access_token, post_title):
     """Search for a WordPress.com post by title."""
-    url = f'https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts'
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-    params = {
-        'search': post_title,
-        'number': 1  # Limit results to optimize the lookup
-    }
-    response = requests.get(url, headers=headers, params=params)
-
+    params = {"search": post_title, "number": 1}  # Limit results to optimize the lookup
+    response = get_wordpress_posts(site_id, access_token, params, paginate=False)
     if response.status_code == 200:
-        posts = response.json().get('posts')
+        posts = response.json().get("posts")
         if posts:
             return posts[0]  # Return the first matched post
         else:
@@ -83,84 +82,143 @@ def get_post_by_title(site_id, access_token, post_title):
         return None
 
 
+def get_wordpress_posts(
+    site_id, access_token, params={}, paginate=True, per_page=10, status="draft"
+):
+    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+    }
+    params = {**{"per_page": per_page, "status": status}, **params}
+    if not paginate:
+        return requests.get(url, headers=headers, params=params)
+    else:
+        initial_response = requests.get(url, headers=headers, params=params)
+        initial_response.raise_for_status()  # Raise an exception for bad status codes
+
+        total_posts = initial_response.json().get("found", 0)
+        total_pages = 1  # default value
+        if total_posts:
+            total_pages = int(total_posts / per_page)
+        else:
+            total_pages = int(initial_response.headers.get("X-WP-TotalPages", "1"))
+
+        print(f"Total Pages: {total_pages}")
+
+        # Make requests for each page and process the results
+        all_posts = []
+        for page in range(1, total_pages + 1):
+            params["page"] = page
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()  # Raise an exception for bad status codes
+            posts = response.json().get("posts", [])
+            all_posts.extend(posts)
+            print(f"Page {page} retrieved")
+        return all_posts
+
+
+def get_wordpress_media(
+    site_id, access_token, params={}, paginate=True, per_page=10
+):
+    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/media"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+    }
+    params = {**{"per_page": per_page}, **params}
+    if not paginate:
+        return requests.get(url, headers=headers, params=params)
+    else:
+        initial_response = requests.get(url, headers=headers, params=params)
+        initial_response.raise_for_status()  # Raise an exception for bad status codes
+
+        total_media = initial_response.json().get("found", 0)
+        total_pages = 1  # default value
+        if total_media:
+            total_pages = int(total_media / per_page)
+        else:
+            total_pages = int(initial_response.headers.get("X-WP-TotalPages", "1"))
+
+        print(f"Total Pages: {total_pages}")
+
+        # Make requests for each page and process the results
+        all_media = []
+        for page in range(1, total_pages + 1):
+            params["page"] = page
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()  # Raise an exception for bad status codes
+            media = response.json().get("media", [])
+            all_media.extend(media)
+            print(f"Page {page} retrieved")
+        return all_media
+
+
 def get_wordpress_post_content(site_id, access_token, post_id):
     """Retrieve content and media from a WordPress.com post."""
-    url = f'https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts/{post_id}'
+    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts/{post_id}"
     headers = {
-        'Authorization': f'Bearer {access_token}',
+        "Authorization": f"Bearer {access_token}",
     }
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         post = response.json()
-        content = post.get('content')
+        content = post.get("content")
         media_urls = []
-        attachments = post.get('attachments', {})
+        attachments = post.get("attachments", {})
         for attachment in attachments.values():
-            media_urls.append(attachment.get('URL'))
+            media_urls.append(attachment.get("URL"))
         return content, media_urls
     else:
         print(
-            f"Failed to retrieve WordPress post: {response.status_code} - {response.text}")
-        return None, None
-
-
-def get_wordpress_post_content(site_id, access_token, post_id):
-    """Retrieve content and media from a WordPress.com post."""
-    url = f'https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts/{post_id}'
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        post = response.json()
-        content = post.get('content')
-        media_urls = []
-        attachments = post.get('attachments', {})
-        for attachment in attachments.values():
-            media_urls.append(attachment.get('URL'))
-        return content, media_urls
-    else:
-        print(
-            f"Failed to retrieve WordPress post: {response.status_code} - {response.text}")
+            f"Failed to retrieve WordPress post: {response.status_code} - {response.text}"
+        )
         return None, None
 
 
 def get_facebook_post_content(post_id, access_token):
     """Retrieve content and media from a Facebook post."""
-    url = f'https://graph.facebook.com/v22.0/{post_id}'
-    params = {
-        'access_token': access_token,
-        'fields': 'message,attachments{media}'
-    }
+    url = f"https://graph.facebook.com/v22.0/{post_id}"
+    params = {"access_token": access_token, "fields": "message,attachments{media}"}
 
     response = requests.get(url, params=params)
     if response.status_code == 200:
         post = response.json()
-        content = post.get('message', '')
+        content = post.get("message", "")
         media_urls = []
-        attachments = post.get('attachments', {}).get('data', [])
+        attachments = post.get("attachments", {}).get("data", [])
         for attachment in attachments:
-            media = attachment.get('media', {}).get('image', {}).get('src')
+            media = attachment.get("media", {}).get("image", {}).get("src")
             if media:
                 media_urls.append(media)
         return content, media_urls
     else:
         print(
-            f"Failed to retrieve Facebook post: {response.status_code} - {response.text}")
+            f"Failed to retrieve Facebook post: {response.status_code} - {response.text}"
+        )
         return None, None
+
+
+def strip_post_text(text):
+    stext = (
+        html.unescape(text)
+        .strip()
+        .replace("<p>", "")
+        .replace("</p>", "")
+        .replace("\n", "")
+    )
+    return "".join(e for e in stext if e.isalnum())
+
 
 def compare_posts(wp_content, wp_media, fb_content, fb_media):
     """Compare the content and media from WordPress and Facebook posts."""
-    text_match = wp_content.strip() == fb_content.strip()
+    text_match = strip_post_text(wp_content) == strip_post_text(fb_content)
     media_match = set(wp_media) == set(fb_media)
     return text_match, media_match
 
 
-def compare_post_content(wp_content, fb_content, fb_media):
+def compare_post_content(wp_content, fb_content):
     """Compare the content and media from WordPress and Facebook posts."""
-    text_match = wp_content.strip() == fb_content.strip()
+    text_match = strip_post_text(wp_content) == strip_post_text(fb_content)
     return text_match
 
 
@@ -195,46 +253,24 @@ def upload_image_to_wordpress(
         raise Exception(f"Failed to upload image to WordPress: {response.status_code}")
 
 
-def upload_image_to_wordpress_com(access_token, site_id, image_path):
-    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/media/new"
-
-    try:
-        with open(image_path, "rb") as img_file:
-            headers = {
-                "Authorization": f"Bearer {access_token}",
-            }
-            files = {
-                "media[]": img_file,
-            }
-            response = requests.post(url, headers=headers, files=files)
-        if response.status_code == 201:
-            print("Upload successful:", response.json())
-            return response.json()
-        else:
-            print("Upload failed:", response.status_code, response.text)
-
-    except FileNotFoundError:
-        print(f"The file {image_path} was not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
 def attach_image_to_post(site_id, access_token, post_id, image_id):
     """Attach an uploaded image to a WordPress.com post."""
-    url = f'https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts/{post_id}'
+    url = f"https://public-api.wordpress.com/rest/v1.1/sites/{site_id}/posts/{post_id}"
     headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
     }
-    data = {
-        'content': f'<img src="{image_id}" alt="Facebook Image">'
-    }
+
+    content = get_wordpress_post_content(site_id, access_token, post_id)
+    data = {"content": f'{content}<img src="{image_id}" alt="Facebook Image">'}
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
         print("Image successfully attached to post.")
     else:
-        print(f"Failed to attach image to post: {response.status_code} - {response.text}")
+        print(
+            f"Failed to attach image to post: {response.status_code} - {response.text}"
+        )
 
 
 def upload_image_to_wordpress_com(access_token, site_id, image_path):
@@ -249,8 +285,9 @@ def upload_image_to_wordpress_com(access_token, site_id, image_path):
                 "media[]": img_file,
             }
             response = requests.post(url, headers=headers, files=files)
-        if response.status_code == 201:
-            print("Upload successful:", response.json())
+        if response.status_code == 201 or response.status_code == 200:
+            # print("Upload successful:", response.json())
+            return response.json().get('media',[])[0].get('ID')
         else:
             print("Upload failed:", response.status_code, response.text)
 
@@ -295,22 +332,86 @@ def check_path_type(path):
         return None
 
 
-def process_facebook_posts(wp_post_id, fb_posts, wp_site_id, wp_access_token):
-    """Process each Facebook post for comparison and upload."""
-    wp_content, wp_media = get_wordpress_post_content(wp_site_id, wp_access_token, wp_post_id)
+def flatten_post_data(posts):
+    for post in posts:
+        post_content = None
+        for d in post.get("data", []):
+            if "post" in d:
+                content = d["post"]
+                if post_content is None:
+                    post_content = content
+                else:
+                    "\n".join([post_content, content])
+        post["content"] = post_content
+    return posts
 
+
+def find_file(directory, filename):
+    for root, dirs, files in os.walk(directory):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+
+def process_facebook_posts(
+    fb_posts,
+    wp_site_id,
+    wp_access_token,
+    wp_post={},
+    wp_media=[],
+    wp_post_id=None,
+    dirname=None,
+):
+    """Process each Facebook post for comparison and upload."""
+    if wp_post_id:
+        wp_content, wp_media = get_wordpress_post_content(
+            wp_site_id, wp_access_token, wp_post_id
+        )
+
+    fb_posts = flatten_post_data(fb_posts)
+    wp_content = wp_post.get("content")
+    wp_post_id = wp_post.get('ID')
+    wp_media = get_wordpress_media(site_id=wp_site_id, access_token=wp_access_token)
     if wp_content is not None:
         for fb_post in fb_posts:
-            fb_content = fb_post.get('message', '')
-            fb_media = [media.get('media', {}).get('image', {}).get('src') for media in fb_post.get('attachments', {}).get('data', []) if media.get('media', {}).get('image', {}).get('src')]
+            fb_content = fb_post.get("content", "")
+            if fb_content is None:
+                fb_content = ""
+            fb_media = []
+            for media in fb_post.get("attachments", []):
+                for data in media.get("data", []):
+                    if data.get("media", {}).get("backup_uri"):
+                        fb_media.append(data.get("media", {}).get("backup_uri"))
 
-            if compare_post_content(wp_content, fb_content, fb_media):
+            if compare_post_content(wp_content, fb_content):
                 print("Content matches. Proceeding to upload images...")
                 for fb_media_url in fb_media:
-                    image_id = upload_image_to_wordpress_com(wp_site_id, wp_access_token, fb_media_url)
+                    if fb_media_url is None:
+                        continue
+                    link_path = urllib3.util.parse_url(fb_media_url).path
+                    link_filename = os.path.basename(link_path)
+                    file_path = find_file(dirname, link_filename)
+                    if file_path is None:
+                        print(f"Could not find file {link_filename} in {dirname}")
+                        continue
+                    print(f"Found file: {file_path}")
+                    print(f"Checking if file exists.")
+                    for m in media:
+                        if m.get('title') == link_filename
+                    print("Uploading...")
+                    image_id = upload_image_to_wordpress_com(
+                        site_id=wp_site_id,
+                        access_token=wp_access_token,
+                        image_path=file_path,
+                    )
                     if image_id:
-                        attach_image_to_post(site_id=wp_site_id, access_token=wp_access_token, post_id=wp_post_id, image_id=image_id)
-
+                        print(f"Attaching image {image_id} to post {wp_post_id}")
+                        attach_image_to_post(
+                            site_id=wp_site_id,
+                            access_token=wp_access_token,
+                            post_id=wp_post_id,
+                            image_id=image_id,
+                        )
 
 
 def main():
@@ -318,28 +419,64 @@ def main():
     facebook_image_url = args.image
     wordpress_post_id = args.post
     site_url = args.url
-    username = args.user
-    password = args.password
+    # username = args.user
+    # password = args.password
     access_token = args.access_token
     export_file = args.export_file
     image_info = {}
+    site_id = None
     if site_url and "wordpress.com" in site_url:
         site_id = get_wordpress_com_site_id(site_url, access_token)
-    if facebook_image_url:
-        # Download the image from Facebook
-        image_data = download_facebook_image(facebook_image_url)
+    if export_file:
+        with open(export_file) as f:
+            fb_posts = json.load(f)
+        wordpress_post_id_list = []
+        if wordpress_post_id:
+            process_facebook_posts(
+                wordpress_post_id,
+                fb_posts,
+                site_id,
+                access_token,
+                dirname=args.photo_dir,
+            )
+        else:
+            if args.wordpress_posts_file:
+                with open(args.wordpress_posts_file) as f:
+                    wordpress_posts = json.load(f)
+            else:
+                wordpress_posts = get_wordpress_posts(
+                    site_id, access_token, paginate=True, per_page=50
+                )
+            for wp_post in wordpress_posts:
+                process_facebook_posts(
+                    fb_posts,
+                    site_id,
+                    access_token,
+                    wp_post=wp_post,
+                    dirname=args.photo_dir,
+                )
+    else:
+        if facebook_image_url:
+            # Download the image from Facebook
+            image_data = download_facebook_image(facebook_image_url)
 
-        # Upload image to WordPress
-        image_name = "uploaded_facebook_image.jpg"  # replace with a suitable file name
-        image_info = upload_image_to_wordpress(image_data, image_name)
-    if args.image:
-        image_info = upload_image_to_wordpress_com(
-            access_token=access_token, site_id=site_id, image_path=args.image
+            # Upload image to WordPress
+            image_name = (
+                "uploaded_facebook_image.jpg"  # replace with a suitable file name
+            )
+            image_info = upload_image_to_wordpress(image_data, image_name)
+        if args.image:
+            image_info = upload_image_to_wordpress_com(
+                access_token=access_token, site_id=site_id, image_path=args.image
+            )
+        # Attach the uploaded image to a WordPress post
+        image_id = image_info.get("id")
+        attach_image_to_post(
+            site_id=site_id,
+            access_token=access_token,
+            post_id=wordpress_post_id,
+            image_id=image_id,
         )
-
-    # Attach the uploaded image to a WordPress post
-    image_id = image_info.get("id")
-    attach_image_to_post(image_id, wordpress_post_id, site_url, username, password)
 
 
 if __name__ == "__main__":
